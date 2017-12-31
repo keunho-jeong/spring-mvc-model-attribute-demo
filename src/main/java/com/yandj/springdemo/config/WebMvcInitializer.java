03d706faba255e14dcb43f8076a045cc83f00a13
keunho.jeong <keunho.jeong@gmail.com>
2018-01-01 10:03:28 +1100

    comment

--- src/main/java/com/yandj/springdemo/config/WebMvcInitializer.java
+++ src/main/java/com/yandj/springdemo/config/WebMvcInitializer.java
@@ -8,8 +8,16 @@ import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
+/**
+ * @author SIRI
+ *
+ */
+
 public class WebMvcInitializer implements WebApplicationInitializer {
 
+	/* (non-Javadoc)
+	 * @see org.springframework.web.WebApplicationInitializer#onStartup(javax.servlet.ServletContext)
+	 */
 	@Override
 	public void onStartup(ServletContext servletContext) throws ServletException {
 		AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
